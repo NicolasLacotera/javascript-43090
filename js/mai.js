cc3bee611bfd8230d3f11f541a0311b7a3fabb7f
NicolasLacotera <nlacotera@gmail.com>
2023-05-05 11:55:32 -0300

    realice pequeña modificacion. cambie el switch que estaba dentro del switch inicial, por un if

--- js/mai.js
+++ js/mai.js
@@ -4,48 +4,36 @@ let consulta = prompt("Hola " + nombre + " cual es el tratamiento de tu interes?
 switch (consulta) {
     case "1":
         let turno = prompt("El valor de este tratamiento es de 50.000$." + "\nDesea pedir un turno?" + "\n1- Si" + "\n2-No");
-        switch (turno) {
-            case "1":
-                let celuturno = prompt("Para turnos comuniquese al 0800-100-555 o deje su numero de celular y nos comunicaremos con usted.")
-                console.log(celuturno);
-                break;
-            case "2":
-                alert("Muchas gracias por su consulta.")
-            default:
-                break;
+        if (turno == "1" || turno == "si") {
+            prompt("Para turnos comuniquese al 0800-100-555 o deje su numero de celular y nos comunicaremos con usted.")
+    
+    
+        } else {
+            alert("Muchas gracias por su consulta.")
         }
-        break;
+
+break;
 
     case "2":
-        let turno1 = prompt("El valor de este tratamiento es de 15.000$." + "\nDesea pedir un turno?" + "\n1- Si" + "\n2-No");
-        switch (turno1) {
-            case "1":
-                let celuturno = prompt("Para turnos comuniquese al 0800-100-555 o deje su numero de celular y nos comunicaremos con usted.")
-                console.log(celuturno);
-                break;
-            case "2":
-                alert("Muchas gracias por su consulta.")
-            default:
-                break;
-        }
-        break;
-    case "3":
-        let turno2 = prompt("El valor de este tratamiento es de 5.000$ ." + "\nDesea pedir un turno?" + "\n1- Si" + "\n2-No");
-        switch (turno2) {
-            case "1":
-                let celuturno = prompt("Para turnos comuniquese al 0800-100-555 o deje su numero de celular y nos comunicaremos con usted.")
-                console.log(celuturno);
-                break;
-            case "2":
-                alert("Muchas gracias por su consulta.")
+let turno1 = prompt("El valor de este tratamiento es de 15.000$." + "\nDesea pedir un turno?" + "\n1- Si" + "\n2-No");
+if (turno1 == "1" || turno1 == "si") {
+    prompt("Para turnos comuniquese al 0800-100-555 o deje su numero de celular y nos comunicaremos con usted.")
+} else {
+    alert("Muchas gracias por su consulta.")
+}
 
-                break;
-            default:
-                break;
-        }
-        break;
+break;
+
+    case "3":
+let turno2 = prompt("El valor de este tratamiento es de 5.000$ ." + "\nDesea pedir un turno?" + "\n1- Si" + "\n2-No");
+if (turno2 == "1" || turno2 == "si") {
+    prompt("Para turnos comuniquese al 0800-100-555 o deje su numero de celular y nos comunicaremos con usted.")
+} else {
+    alert("Muchas gracias por su consulta.")
+}
+break;
 
     default:
-        console.log("Opción no válida");
-        break;
+console.log("Opción no válida");
+break;
 }
